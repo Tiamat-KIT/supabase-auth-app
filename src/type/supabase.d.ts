d9fdc41865b85da7fe9791649ab7ba8cf0889021
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-11-10 05:57:54 +0000

    保存

--- src/type/supabase.d.ts
+++ src/type/supabase.d.ts
@@ -1,10 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
+import { Database } from './schema';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 declare global {
-  var supabaseClient: SupabaseClient<any, 'public', any> | undefined;
+  var supabaseClient: SupabaseClient<Database> | undefined;
 }
 
 const supabaseClient =
